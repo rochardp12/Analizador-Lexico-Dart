830f59d9162de2e1e4c38c58095f97cb399c1feb
unknown <ricluper@espol.edu.ec>
2024-06-14 18:45:39 -0500

    algoritmo y correcciones en expresiones regulares

--- Analizador Lexico Dart.py
+++ Analizador Lexico Dart.py
@@ -35,15 +35,16 @@ tokens = (
     'DOT',
     'COMMENT',
     'STRINGTEXT',
-    'NEWDATATYPE'
+    'NEWDATATYPE',
     'INTERNDATATYPE',
     'COMMA',
-    'NUMBER',
+    'NUMBERINT',
     'BOOLTEXT',
     'DATAATTRIBUTE',
     'COLON',
-    'ADDEDSTRING',
-    'DATAFUNCTION'
+    'DATAFUNCTION',
+    'ASSIGN',
+    'FUNCTION'
     )+tuple(reserved.values())
 
 # Regular expression rules for simple tokens
@@ -58,12 +59,32 @@ t_ARROWFUNCTION = r'=>'
 t_DOT = r'\.'
 t_COMMA = r','
 t_COLON = r':'
+t_ASSIGN = r'='
 
 #Richard Perez
+
 def t_NEWDATATYPE(t):
-  r'[A-Z][_a-zA-Z0-9]*'
-  t.type = reserved.get(t.value, 'NEWDATATYPE')
-  return t
+    r'[A-Z][_a-zA-Z0-9]*'
+    t.type = reserved.get(t.value, 'NEWDATATYPE')
+    return t
+
+def t_FUNCTION(t):
+    r'[a-zA-Z]+\w*\(\w*\)'
+    t.type = reserved.get(t.value, 'FUNCTION')
+    return t
+
+def t_DATAFUNCTION(t):
+    r'\.[a-zA-Z]+\w*\(\w*\)'
+    t.type = reserved.get(t.value, 'DATAFUNCTION')
+    return t
+
+def t_BOOLTEXT(t):
+    r'true|false'
+    if t.value == 'true':
+        t.value = True
+    else:
+        t.value = False    
+    return t
 
 def t_VARIABLE(t):
     r'[_a-zA-Z]\w*'
@@ -75,44 +96,28 @@ def t_COMMENT(t):
     t.type = reserved.get(t.value, 'COMMENT')
     return t
 
-def t_ADDEDSTRING(t):
-    r'${\w+}'
-    t.type = reserved.get(t.value, 'ADDEDSTRING')
-    return t
-
 def t_STRINGTEXT(t):
     r'"[^"]*"'
     t.type = reserved.get(t.value, 'STRINGTEXT')
     return t
 
 def t_INTERNDATATYPE(t):
-    r'<[A-Z][_a-zA-Z0-9]*>'
+    r'\<[A-Z][_a-zA-Z0-9]*\>'
     t.type = reserved.get(t.value, 'INTERNDATATYPE')
     return t
 
 def t_DATAATTRIBUTE(t):
-    r'\.[a-z0-9]+'
+    r'\.[a-zA-Z_]+\w*'
     t.type = reserved.get(t.value, 'DATAATTRIBUTE')
     return t
 
-def t_DATAFUNCTION(t):
-    r'\.[a-z0-9]+(\w*)'
-    t.type = reserved.get(t.value, 'DATAFUNCTION')
-    return t
 
 # A regular expression rule with some action code
-def t_NUMBER(t):
+def t_NUMBERINT(t):
     r'\d+'
     t.value = int(t.value)    
     return t
 
-def t_BOOLTEXT(t):
-    r'true|false'
-    if t.value == 'true':
-        t.value = True
-    else:
-        t.value = False    
-    return t
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -130,13 +135,43 @@ def t_error(t):
 # Build the lexer
 lexer = lex.lex()
 
-data = """def function(a, b):
-4*5=4+t
-%6
-.257
-print(a)
-PRINT(B)
-return b"""
+data = """
+        class Pais {
+        String nombre;
+        bool ganoMundial;
+        int cantidad;
+
+        Pais(this.nombre, this.ganoMundial, this.cantidad);
+
+        public String cantidadMundiales() => print(this.cantidad)
+        }
+
+        void main() {
+        // Crear un conjunto de países
+        Set<Pais> paises = {
+            Pais("Brasil", true, 5),
+            Pais("Argentina", true, 3),
+            Pais("España", true, 1),
+            Pais("Italia", true, 4),
+            Pais("México", false, 0),
+            Pais("Japón", false, 0),
+        };
+        // Recorrer el conjunto de países y aplicar el switch en ganoMundial
+        for (Pais pais in paises) {
+            switch (pais.ganoMundial) {
+            case true:
+                print("GANÓ:  ${pais.nombre}");
+                pais.cantidadMundiales();
+                break;
+            case false:
+                print("NO GANÓ:  ${pais.nombre}");
+                break;
+            case default:
+                print("Sin Informacion");
+            }
+        }
+        }
+        """
 
 lexer.input(data)
 
