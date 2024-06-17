bdc4777e6edc7765bdfb48dc6b15778ad5f6ee71
roberto encalada <rocaenca@espol.edu.ec>
2024-06-16 20:27:14 -0500

    Analizador Léxico Final

--- Analizador Lexico Dart.py
+++ Analizador Lexico Dart.py
@@ -4,14 +4,11 @@ from datetime import datetime
 
 # List of token names.   This is always required
 reserved = {
+    #Roberto Encalada
     'main': 'MAIN',
     'new': 'NEW',
-    'this': 'THIS',
     'null': 'NULL',
-    'list': 'LIST',
     'map': 'MAP',
-    'class': 'CLASS',
-    'public': 'PUBLIC',
     'private': 'PRIVATE',
     'protected': 'PROTECTED',
     'static': 'STATIC',
@@ -21,26 +18,16 @@ reserved = {
     'module': 'MODULE',
     'def': 'DEF',
     'method': "METHOD", 
-    'void': 'VOID', 
-    'var': 'VAR', 
-    'string':'STRING', 
-    'int':'INT',
     'final':'FINAL',
     'dynamic':'DYNAMIC',
     'double':'DOUBLE',
     'const':'CONST',
-    'boolean':'BOOLEAN',
     'typedef':'TYPEDEF',
-    'print':'PRINT',
-    'for':'FOR',
     'while':'WHILE',
     'if':'IF',
     'else':'ELSE',
     'elif':'ELIF',
     'return':'RETURN',
-    'break':'BREAK',
-    'switch':'SWITCH',
-    'case':'CASE',
     'try':'TRY',
     'catch':'CATCH',
     'finally':'FINALLY',
@@ -48,37 +35,29 @@ reserved = {
     'False':'FALSE',
     'as':'AS',
     'is':'IS',
-    'enum':'ENUM',
     'super':'SUPER',
-       #Richard Perez
+    #Richard Perez
     "class": "CLASS",
     "String": "STRING",
-    "bool": "BOOL",
     "int": "INT",
     "this": "THIS",
     "public": "PUBLIC",
     "print": "PRINT",
-    "void": "VOID",
-    "main": "MAIN",
     "Set": "SET",
-    "for": "FOR",
-    "in": "IN",
     "switch": "SWITCH",
     "case": "CASE",
-    "break": "BREAK",
-    "default": "DEFAULT"
+    "default": "DEFAULT",
     #Katherine Tumbaco
     "enum": "ENUM",
     "void": "VOID",
-    "main": "MAIN",
     "for": "FOR",
     "var": "VAR",
     "in": "IN",
     "break": "BREAK",
-    "List": "LIST",
-      
+    "List": "LIST"
     }
 tokens = (
+    #Katherine Tumbaco
     'NUMBER',
     'FLOAT',
     'PLUS',
@@ -93,6 +72,7 @@ tokens = (
     'RBRACKET',
     'LANGLE',
     'RANGLE',
+    #Roberto Encalada
     'MOD',
     'INTEGERDIVISION',
     'VARIABLE',
@@ -109,47 +89,19 @@ tokens = (
     'commentLine',
     'commentBlock',
     'CHAINCHAR',
-  #Richard Perez
-    'VARIABLE',
-    'COMA',
-    'TWOPOINTS',
-    'FLOAT',
-    #Katherine Tumbaco
-   'LLlave',
-   'RLlave',
-   'LCORC',
-   'RCORC',
-   'SEMICOLON',
-   'EQUALS',
-   'POINT',
-   'Lmenor',
-   'Rmayor'
-    'LCURLYBRACE',
-    'RCURLYBRACE',
-    'LPAREN',
-    'RPAREN',
-    'SEMICOLON',
+    #Richard Perez
     'ARROWFUNCTION',
-    'DOT',
-    'COMMENT',
-    'STRINGTEXT',
     'NEWDATATYPE',
     'INTERNDATATYPE',
-    'COMMA',
     'NUMBERINT',
-    'BOOLTEXT',
     'DATAATTRIBUTE',
-    'COLON',
     'DATAFUNCTION',
-    'ASSIGN',
     'FUNCTION'
 ) + tuple(reserved.values())
 
 # Regular expression rules for simple tokens
-t_MOD     = r'%'
-t_INTEGERDIVISION = r'~/'
-t_COMMA   = r','
-t_EQUALS  = r'='
+
+#Katherine Tumbaco
 t_NEQ = r'!='
 t_DOT = r'\.'
 t_TWODOTS = r':'
@@ -157,7 +109,13 @@ t_LBRACE  = r'\{'
 t_RBRACE  = r'\}'
 t_LBRACKET = r'\['
 t_RBRACKET = r'\]'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
 t_DOTCOMMA = r';'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_MINUS = r'-'
+#Roberto Encalada
 t_LANGLE = r'<'
 t_RANGLE = r'>'
 t_ADMIRATION = r'!'
@@ -168,36 +126,14 @@ t_commentLine = r'//.*'
 t_commentBlock = r'/\*(.|\n)*?\*/'
 t_CHAINCHAR = r'(\'[^\']*\'|\"[^\"]*\")'
 #Richard Perez
-t_LCURLYBRACE = r'\{'
-t_RCURLYBRACE = r'\}'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_MOD = r'%'
-t_COMA = r','
-t_TWOPOINTS = r':'
-#Katherine Tumbaco
-t_LLlave  = r'\{'
-t_RLlave  = r'\}'
-t_Lmenor   = r'\<'
-t_Rmayor   = r'\>'
-t_LCORC = r'\['
-t_RCORC = r'\]'
-t_SEMICOLON = r';'
-t_EQUALS   = r'='
-t_POINT      = r'\.'
-t_SEMICOLON = r';'
+t_MOD     = r'\%'
+t_INTEGERDIVISION = r'~/'
+t_COMMA   = r','
+t_EQUALS  = r'='
 t_ARROWFUNCTION = r'=>'
-t_DOT = r'\.'
-t_COMMA = r','
-t_COLON = r':'
-t_ASSIGN = r'='
+t_PLUS    = r'\+'
 
-def t_VARIABLE(t):
-    r'[a-zA-Z_]\w*'
-    t.type = reserved.get(t.value,'VARIABLE')    # Check for reserved words
-    return t
- #Richard Perez
-
+#Richard Perez
 def t_NEWDATATYPE(t):
     r'[A-Z][_a-zA-Z0-9]*'
     t.type = reserved.get(t.value, 'NEWDATATYPE')
@@ -213,25 +149,7 @@ def t_DATAFUNCTION(t):
     t.type = reserved.get(t.value, 'DATAFUNCTION')
     return t
 
-def t_BOOLTEXT(t):
-    r'true|false'
-    if t.value == 'true':
-        t.value = True
-    else:
-        t.value = False    
-    return t 
-
-
-def t_COMMENT(t):
-    r'//.*'
-    t.type = reserved.get(t.value, 'COMMENT')
-    return t
-
-def t_STRINGTEXT(t):
-    r'"[^"]*"'
-    t.type = reserved.get(t.value, 'STRINGTEXT')
-    return t
-
+#Roberto Encalada
 def t_INTERNDATATYPE(t):
     r'\<[A-Z][_a-zA-Z0-9]*\>'
     t.type = reserved.get(t.value, 'INTERNDATATYPE')
@@ -242,15 +160,7 @@ def t_DATAATTRIBUTE(t):
     t.type = reserved.get(t.value, 'DATAATTRIBUTE')
     return t
 
-
-# A regular expression rule with some action code
-def t_NUMBERINT(t):
-    r'\d+'
-    t.value = int(t.value)    
-    return t
-
-# A regular expression rule with some action code
-
+#Katherine Tumbaco
 def t_FLOAT(t):
     r'\d+\.\d+'
     t.value = float(t.value)
@@ -260,13 +170,16 @@ def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
-    
-# Define a rule so we can track line numbers
+
+def t_VARIABLE(t):
+    r'[a-zA-Z_]\w*'
+    t.type = reserved.get(t.value,'VARIABLE')    # Check for reserved words
+    return t
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# A string containing ignored characters (spaces and tabs)
 t_ignore  = ' \t'
 
 # Error handling rule
@@ -274,24 +187,11 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-# Build the lexer
-lexer = lex.lex()
 
-#Generate logs
-log_dir = "logs"
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
-usuarioGitKatherine = "katumbac"
-usuarioGitRoberto = "rocaenca"
-usuarioGitRichard = "rochardp12"
-current_time = datetime.now().strftime("%d%m%Y-%Hh%M")
-log_filename = f"lexico-{usuarioGit}-{current_time}.txt"
-log_filepath = os.path.join(log_dir, log_filename)
 
 
 #Katherine Tumbaco
-algoritmoKatherine = '''
+algoritmoKatherine = """
 enum DiaSemana{
     Lunes,
     Martes,
@@ -318,20 +218,12 @@ void main(){
     }
 } 
 
-'''
+"""
 
-# Tokenize
-with open(log_filepath, 'w') as log_file:
-    while True:
-        tok = lexer.token()
-        if not tok: 
-            break      # No more input
-        log_file.write(f"{tok}\n")
-        print(tok)
 algoritmoRichard = """
         class Pais {
         String nombre;
-        bool ganoMundial;
+        boolean ganoMundial;
         int cantidad;
 
         Pais(this.nombre, this.ganoMundial, this.cantidad);
@@ -342,21 +234,21 @@ algoritmoRichard = """
         void main() {
         // Crear un conjunto de países
         Set<Pais> paises = {
-            Pais("Brasil", true, 5),
-            Pais("Argentina", true, 3),
-            Pais("España", true, 1),
-            Pais("Italia", true, 4),
-            Pais("México", false, 0),
-            Pais("Japón", false, 0),
+            Pais("Brasil", True, 5),
+            Pais("Argentina", True, 3),
+            Pais("España", True, 1),
+            Pais("Italia", True, 4),
+            Pais("México", False, 0),
+            Pais("Japón", False, 0),
         };
         // Recorrer el conjunto de países y aplicar el switch en ganoMundial
         for (Pais pais in paises) {
             switch (pais.ganoMundial) {
-            case true:
+            case True:
                 print("GANÓ:  ${pais.nombre}");
                 pais.cantidadMundiales();
                 break;
-            case false:
+            case False:
                 print("NO GANÓ:  ${pais.nombre}");
                 break;
             case default:
@@ -374,7 +266,7 @@ This is a test for the block comment token.
 public class TestClass {
     private int number = 42;
     public double decimalNumber = 3.14;
-    protected boolean isValid = true;
+    protected isValid = True;
     static final String CONSTANT_STRING = "Hello";
     private List<Integer> myList = new ArrayList<>();
     private Map<String, Integer> myMap = new HashMap<>();
@@ -387,11 +279,11 @@ public class TestClass {
     public void testMethod() {
         int localVar = 10;
         double pi = 3.14;
-        boolean flag = false;
+        eanean flag = False;
         String greeting = "Hello, World!";
         char initial = 'A';
         
-        if (localVar > 5 && flag == false) {
+        if (localVar > 5 && flag == False) {
             print("localVar is greater than 5");
         } else {
             print("localVar is less than or equal to 5");
@@ -455,16 +347,29 @@ public class TestClass {
         System.out.println(message);
     }
 }
-
 """
-lexer.input(algoritmoKatherine)
-lexer.input(AlgoritmoRoberto)
+algoritmos = [algoritmoKatherine, algoritmoRichard, AlgoritmoRoberto]
+# Build the lexer
+lexer = lex.lex()
 
-# Tokenize
-with open(log_filepath, 'w') as log_file:
-  while True:
-      tok = lexer.token()
-      if not tok: 
-          break      # No more input
-      print(tok)
-      log_file.write(f"{tok}\n")
+#Generate logs
+log_dir = "logs"
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+UsuariosGit = ["katumbac", "rochardp12", "rocaenca"]
+current_time = datetime.now().strftime("%d%m%Y-%Hh%M")
+
+
+for i in range(len(algoritmos)):
+  log_filename = f"lexico-{UsuariosGit[i]}-{current_time}.txt"
+  log_filepath = os.path.join(log_dir, log_filename)
+  lexer.input(algoritmos[i])
+  # Tokenize
+  with open(log_filepath, 'w') as log_file:
+    while True:
+        tok = lexer.token()
+        if not tok: 
+            break      # No more input
+        print(tok)
+        log_file.write(f"{tok}\n")
